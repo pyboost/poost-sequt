804481eb4723150be77078d88e1c6f545e8e5d15
Pengkui Luo <pengkui.luo@gmail.com>
2013-07-11 20:50:34 -0700

    .gitignore

---
+++ merging.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Pengkui Luo <pengkui.luo@gmail.com>
+# Created 07/10/2013, updated 07/10/2013
+#
+""" Given a sequence of sequences (e.g. a list of lists/sets),
+    provide some routines for merging these sequences
+    based on different criteria.
+"""
+__all__ = [
+
+]
+print('Executing %s' %  __file__)
+
+import os, sys, time
+
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    print('Done. Time elapsed: %.2f.' % (time.time() - t0))
